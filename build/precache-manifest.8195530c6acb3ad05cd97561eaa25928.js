98b5c88f272afdfbb6ca80394c83e731bf9b8370
maggiemoteno2 <maggiemoteno@gmail.com>
2020-11-11 12:15:33 +0200

    changes

---
+++ build/precache-manifest.8195530c6acb3ad05cd97561eaa25928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc290c0518620cd0068fa15f490858d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9095e3605f409956d2ae",
+    "url": "/static/css/main.5ff28b28.chunk.css"
+  },
+  {
+    "revision": "d6794b2f68fd05445cdc",
+    "url": "/static/js/2.22782ba0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22782ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9095e3605f409956d2ae",
+    "url": "/static/js/main.8d468257.chunk.js"
+  },
+  {
+    "revision": "58228b2e5bf98f14c815",
+    "url": "/static/js/runtime-main.dd3daeed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
